4da1793cb2a24f7b18084f259a9dd279ccc21d38
Konstantinos <kongallis@gmail.com>
2020-11-03 11:31:14 +0200

    Encoded the passwords

--- src/main/java/com/kongallis/blog/service/AuthService.java
+++ src/main/java/com/kongallis/blog/service/AuthService.java
@@ -4,6 +4,7 @@ import com.kongallis.blog.dto.RegisterRequest;
 import com.kongallis.blog.models.User;
 import com.kongallis.blog.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -12,14 +13,20 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
 
         // Save object to the database
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
