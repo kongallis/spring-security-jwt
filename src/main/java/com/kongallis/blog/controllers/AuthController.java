5ff45d7f7bfb55ab2af521c5ceee004585173130
Konstantinos <kongallis@gmail.com>
2020-11-03 10:48:34 +0200

    Corrected Spring Security configuration to allow signup requests

--- src/main/java/com/kongallis/blog/controllers/AuthController.java
+++ src/main/java/com/kongallis/blog/controllers/AuthController.java
@@ -1,6 +1,10 @@
 package com.kongallis.blog.controllers;
 
 import com.kongallis.blog.dto.RegisterRequest;
+import com.kongallis.blog.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/auth")
 public class AuthController {
 
-    @PostMapping("/signup")
-    public void signup (@RequestBody RegisterRequest registerRequest) {
+    @Autowired
+    private AuthService authService;
 
+
+    @PostMapping("/signup")
+    public ResponseEntity signup (@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/kongallis/blog/service/AuthService.java
+++ src/main/java/com/kongallis/blog/service/AuthService.java
@@ -2,16 +2,24 @@ package com.kongallis.blog.service;
 
 import com.kongallis.blog.dto.RegisterRequest;
 import com.kongallis.blog.models.User;
+import com.kongallis.blog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class AuthService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
         user.setPassword(registerRequest.getPassword());
         user.setEmail(registerRequest.getEmail());
+
+        // Save object to the database
+        userRepository.save(user);
     }
 }
